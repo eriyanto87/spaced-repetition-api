95eb7a78e3abd2625e7a28fe053bb37198862886
Evisari Riyanto <evisari111012@gmail.com>
2020-12-12 21:43:33 -0800

    day2

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -17,18 +17,20 @@ VALUES
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
-  (1, 'French', 1);
+  (1, 'Indonesian', 1);
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'makan', 'eat', 2),
+  (2, 1, 'tolong', 'help', 3),
+  (3, 1, 'Terima Kasih', 'Thank You', 4),
+  (4, 1, 'bagus', 'good', 5),
+  (5, 1, 'baik', 'fine', 6),
+  (6, 1, 'suka', 'like', 7),
+  (7, 1, 'tidak', 'no', 8),
+  (8, 1, 'mau', 'want', 9), 
+  (9, 1, 'kita', 'we', 10), 
+  (10, 1, 'apa kabar', 'how are you', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
